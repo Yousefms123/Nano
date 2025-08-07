e24c35385da99c6feb7f5c44bb5e62ace67dc8ce
Yousef.MS <youaifms@gmail.com>
2025-08-07 15:11:32 +0300

    Edit the location on the map as well as some content in the context.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -62,7 +62,7 @@ const ContactForm = () => {
 					<div className="pb-5 ">
 						<iframe
 							title="Google Map"
-							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3655.0769367034673!2d58.13999382521145!3d23.63741559310554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e8de3d9badaeb93%3A0x60dd73d261452614!2z2YHZhtiv2YIg2KfZhNmG2KzZhdipINin2YTZhdin2LPZitip!5e0!3m2!1sar!2s!4v1752664643546!5m2!1sar!2s"
+							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3654.9520831951054!2d58.2258622!3d23.641887!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e8dfd389a330f0f%3A0x5171ffff875cb636!2z2YXYsdmD2LIg2KfZhNiz2YrYqCDYp9mE2YLYsdii2YbZiiAtINin2YTYrNmF2LnZitipINin2YTYudmF2KfZhtmK2Kkg2YTZhNi52YbYp9mK2Kkg2KjYp9mE2YLYsdii2YYg2KfZhNmD2LHZitmF!5e0!3m2!1sar!2s!4v1754217694057!5m2!1sar!2s"
 							className="w-full h-[70vh] rounded-lg border-0"
 							style={{ border: 0 }}
 							allowFullScreen=""
